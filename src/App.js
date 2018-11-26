fb389a8bd63181809e0c0b0f2bd15e950e14d64f
Charles McQuain <charlesmcquain@gmail.com>
2018-11-25 19:20:57 -0800

    Added dummy data to const list

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const list = [
+  {
+  title: 'React',
+  url: 'https://reactjs.org/',
+  author: 'Jordan Walke',
+  num_comments: 3,
+  points: 4,
+  objectID: 0,
+  },
+  {
+  title: 'Redux',
+  url: 'https://redux.js.org/',
+  author: 'Dan Abramov, Andrew Clark',
+  num_comments: 2,
+  points: 5,
+  objectID: 1,
+  },
+  ];
+
 class App extends Component {
   render() {
-    var helloWorld = "Welcome to the Road to learn React";
-    var user = {
-      name: 'Charles McQuain',
-      age: 31
-    }
 
     return (
       <div className="App">
-        <h2>{helloWorld}</h2>
-        <p>{user.name}</p>
-        <p>{user.age}</p>
+        {list.map(function(item) {
+          return <div>{item.title}</div>
+        })}
       </div>
     );
   }

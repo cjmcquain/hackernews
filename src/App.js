d05ca628e992c2c72bcc843e395e70d2860232fe
Charles McQuain <charlesmcquain@gmail.com>
2018-11-25 19:01:56 -0800

    Removed bolerplate code from App.js and inserted Starter code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+    var helloWorld = "Welcome to the Road to learn React";
+    var user = {
+      name: 'Charles McQuain',
+      age: 31
+    }
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h2>{helloWorld}</h2>
+        <p>{user.name}</p>
+        <p>{user.age}</p>
       </div>
     );
   }
